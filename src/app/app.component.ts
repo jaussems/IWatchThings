69597f307b90ce5cc2db626b4c2f527fc18d837b
jannesaussems <jannes.aussems@blaud.com>
2023-12-17 21:28:10 +0100

    adding search input to header, removed padding on whole app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HttpClientModule, NgOptimizedImage],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HttpClientModule, NgOptimizedImage, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
